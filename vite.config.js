deb083b06a995694d842b25f366b31727fbc1d22
Trident14 <sekarinesh14@gmail.com>
2025-02-04 12:00:47 +0530

    update vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/',
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),  // Alias '@' to the 'src' folder
-    },
-  },
-});
+})
